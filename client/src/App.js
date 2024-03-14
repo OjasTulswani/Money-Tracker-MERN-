cd5513753949de21c75f1bebb9030931dbaca6ce
Ojas <ojastulswani@gmail.com>
2024-03-14 17:38:46 +0530

    frontend (react js) setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="cotainer">
+      <h1 className="text-center"> HII </h1>
+      
+    </div>
+  );
+}
+
+export default App;
