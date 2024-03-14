5410841b555f94f7dc19c4d774cf691eddbeb5d9
Ojas <ojastulswani@gmail.com>
2024-03-14 16:19:53 +0530

    Route and Controller of User

---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const userModel = requiere('../models/userModel')
+
+// login callback
+const loginController = async (req,res) => {
+    try{
+        const {email, password} = req.body
+        const user = await userModel.findOne({email, password})
+        if(!user){
+            return res.status(404).send("User not found")
+        }
+        response.status(200).json({
+            success: true,
+            user,
+        });
+
+    } catch(err){
+        res.status(400).json({
+            success : false,
+            err
+        });
+    }
+};
+
+// Register callback
+const registerController = async (req,res) => {
+    try {
+        const newUser = new userModel(req.body);
+        await newUser.save();
+        res.status(201).json({
+            success: true,
+            newUser,
+        });
+
+    } catch(err){
+        res.status(400).json({
+            success : false,
+            err,
+        });
+    }
+};
+
+module.exports = { loginController, registerController }; 
