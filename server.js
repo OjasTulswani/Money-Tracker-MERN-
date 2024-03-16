7212b300d43091b8bd6ef82ae3244183fee6f128
Ojas <ojastulswani@gmail.com>
2024-03-16 14:04:41 +0530

    concurrently package setup

--- server.js
+++ server.js
@@ -20,10 +20,7 @@ app.use(express.json())
 app.use(cors())
 
 // routes
-app.get('/', (req,res) =>{
-    res.send(`<h1>Hii from server</h1>`)
-    
-})
+app.use('/api/v1/users', require('./routes/userRoute'))
 
 // port
 const PORT = 8080 || process.env.PORT
