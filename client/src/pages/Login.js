599c7b42c2865904b111e1da447ce110279f705a
Ojas <ojastulswani@gmail.com>
2024-03-19 14:15:31 +0530

    css and hosting config

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from "react";
 import { Form, Input, message } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import "../styles/Loginpage.css";
 import Spinner from "../components/Spinner";
+
 const Login = () => {
+  const img =
+    "https://images.unsplash.com/photo-1593538312308-d4c29d8dc7f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  //from submit
+
   const submitHandler = async (values) => {
     try {
       setLoading(true);
-      const { data } = await axios.post("/users/login", values);
+      const { data } = await axios.post("/api/v1/users/login", values);
       setLoading(false);
       message.success("login success");
       localStorage.setItem(
@@ -24,7 +28,6 @@ const Login = () => {
     }
   };
 
-  //prevent for login user
   useEffect(() => {
     if (localStorage.getItem("user")) {
       navigate("/");
@@ -33,31 +36,34 @@ const Login = () => {
 
   return (
     <>
-    <div className='register-page'>
+      <div className="login-page">
         {loading && <Spinner />}
-            <Form layout='vertical' onFinish={submitHandler}>
-                <h1>Login</h1>
-                
-                
-                <Form.Item label= "Email" name="email">
-                    <Input type='email' />
-                </Form.Item>
-                <Form.Item label= "Password" name="password">
-                    <Input type='password'/>
-                </Form.Item>
-
-                <div className="flex justify-between">
-                    <Link to="/register" >Not a user? Register...</Link>
-                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                        Login
-                    </button>
-                </div>
+        <div className="grid grid-cols-12 container">
+          <h1 className="col-span-12">Money Tracker</h1>
+          <div className="col-span-6">
+            <img src={img} alt="login-img" className="w-full h-full" />
+          </div>
+          <div className="col-span-4 login-form">
+            <Form layout="vertical" onFinish={submitHandler}>
+              <h1>Login</h1>
+              <Form.Item label="Email" name="email">
+                <Input type="email" />
+              </Form.Item>
+              <Form.Item label="Password" name="password">
+                <Input type="password" />
+              </Form.Item>
+              <div className="flex justify-between">
+                <Link to="/register">Not a user? Register...</Link>
+                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                  Login
+                </button>
+              </div>
             </Form>
-
+          </div>
         </div>
-      
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,64 +1,58 @@
-import React, {useState, useEffect} from 'react'
-import {Form, Input, message} from 'antd'
-import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import Spinner from '../components/Spinner'
-
-
+import React, { useState, useEffect } from "react";
+import { Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import "../styles/Register.css"; // Import your custom CSS file for styling
 
 const Register = () => {
-    const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    //form submit
-    const submitHandler = async (values) =>{
-        try {
-            setLoading(true)
-            await axios.post('/users/register', values)
-            message.success('Successfully registered!')
-            setLoading(false)
-            navigate('/login')
-        } catch (error) {
-            setLoading(false)
-            message.error('Something went wrong...')
-        }
-    };
+  // Form submit
+  const submitHandler = async (values) => {
+    try {
+      setLoading(true);
+      await axios.post("/api/v1/users/register", values);
+      message.success("Successfully registered!");
+      setLoading(false);
+      navigate("/login");
+    } catch (error) {
+      setLoading(false);
+      message.error("Something went wrong...");
+    }
+  };
 
-      //prevent for registration user
-        useEffect(() => {
-            if (localStorage.getItem("user")) {
-            navigate("/");
-            }
-        }, [navigate]);
+  // Prevent user from accessing registration if already logged in
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      navigate("/");
+    }
+  }, [navigate]);
 
   return (
-    <>
-        <div className='register-page'>
-            {loading && <Spinner />}
-            <Form layout='vertical' onFinish={submitHandler}>
-                <h1>Register</h1>
-                
-                <Form.Item label= "Name" name="name">
-                    <Input />
-                </Form.Item>
-                <Form.Item label= "Email" name="email">
-                    <Input type='email' />
-                </Form.Item>
-                <Form.Item label= "Password" name="password">
-                    <Input type='password'/>
-                </Form.Item>
-
-                <div className="flex justify-between">
-                    <Link to="/login" >Already Register? Login...</Link>
-                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                        Register
-                    </button>
-                </div>
-            </Form>
-
+    <div className="register-page">
+      {loading && <Spinner />}
+      <Form layout="vertical" onFinish={submitHandler}>
+        <h1 className="font-extrabold text-lg">Register</h1>
+        <Form.Item label="Name" name="name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Email" name="email">
+          <Input type="email" />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input type="password" />
+        </Form.Item>
+        <div className="flex justify-between">
+          <Link to="/login" className="link-to-login">
+            Already Registered? Login...
+          </Link>
+          <button className="register-button">Register</button>
         </div>
-    </>
-  )
-}
+      </Form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
